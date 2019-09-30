588d97c0d97b26fc70f2469d0ca17f18f336d980
Zoltan Matyas <matyesz79@gmail.com>
2019-09-30 17:32:52 +0200

    Adding priority support

--- java/org/networkcalculus/dnc/model/converter/Converter.java
+++ java/org/networkcalculus/dnc/model/converter/Converter.java
@@ -2,8 +2,11 @@ package org.networkcalculus.dnc.model.converter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.networkcalculus.dnc.curves.ArrivalCurve;
 import org.networkcalculus.dnc.curves.Curve;
@@ -13,6 +16,8 @@ import org.networkcalculus.dnc.model.Link;
 import org.networkcalculus.dnc.model.Network;
 import org.networkcalculus.dnc.model.OutPort;
 import org.networkcalculus.dnc.model.Path;
+import org.networkcalculus.dnc.model.ethernet.NetworkInterface;
+import org.networkcalculus.dnc.model.ethernet.VirtualLink;
 import org.networkcalculus.dnc.network.server_graph.Server;
 import org.networkcalculus.dnc.network.server_graph.ServerGraph;
 
@@ -38,7 +43,7 @@ public class Converter {
             //TODO: Must be decided on the model what kind of arrivalCurve to use
             ArrivalCurve arrival_curve = Curve.getFactory().createTokenBucket(rate, burst);
             for (final Path path : flow.getPaths()) {
-                result.addFlow(createName(flow, path), arrival_curve, createPath(path, result, network.getBandwidth()));
+                result.addFlow(createName(flow, path), arrival_curve, createPath(flow, path, result, network));
             }
         }
         return result;
@@ -50,20 +55,21 @@ public class Converter {
         return flow.getName() + "(" + srcName + " - " + dstName + ")";
     }
     
-    private static final List<Server> createPath(final Path path, final ServerGraph graph, final double bandwidth) throws Exception {
+    private static final List<Server> createPath(final Flow flow, final Path path, final ServerGraph graph, final Network network) throws Exception {
         final List<Server> result = new ArrayList<>();
         Server previousServer = null;
         for (final Link link : path.getLinks()) {
             final OutPort port = link.getSrcPort();
             //Outport -> Server
-            Server server = SERVERS.get(port.getName());
+            final String portName = port.getName() + "#PRIO" + flow.getPriority();
+            Server server = SERVERS.get(portName);
             if (server == null) {
-                final ServiceCurve sc = Curve.getFactory().createRateLatency(bandwidth, port.getPort().getDevice().getInternalLatency());
+                final ServiceCurve sc = Curve.getFactory().createRateLatency(calculateBandwidth(flow, port, network), calculateLatency(flow, port, network));
                 server = graph.addServer(port.getName(), sc);
                 //TODO: ??show this on model level??
                 server.useMaxSC(false);
                 server.useMaxScRate(false);
-                SERVERS.put(port.getName(), server);
+                SERVERS.put(portName, server);
             } 
             result.add(server);
             if (previousServer != null) {
@@ -73,5 +79,83 @@ public class Converter {
         }
         return result;
     }
-    
+
+    private static double calculateBandwidth(final Flow flow, final OutPort port, final Network network) {
+        double bandwidth = network.getBandwidth();
+        double result = bandwidth;
+        final Set<Flow> vls = getVLsOnPort(port, network);
+        //Collecting higher prio VLS for delay in rate latency
+        final List<Flow> higherPrioVls = vls.stream().filter(vl -> vl.getPriority() < flow.getPriority()).collect(Collectors.toList());
+        if (higherPrioVls != null && higherPrioVls.size() > 0) {
+            //service curve: R-rate
+            // latency: frameLength/(R-rate)
+            double sumRate = 0.0;
+            for (final var higherPrioVl : higherPrioVls) {
+                double c = higherPrioVl.getMaxLenght()/bandwidth;
+                double t = higherPrioVl.getMinRetransmissionInterval();
+                double rate = (c)/(t) * bandwidth;
+                sumRate += rate;
+            }
+            result = result - sumRate;
+        }
+        return result;
+    }
+
+    private static double calculateLatency(final Flow flow, final OutPort port, final Network network) {
+        final Set<Flow> vls = getVLsOnPort(port, network);
+        //Collecting higher prio VLS for delay in rate latency
+        final List<Flow> higherPrioVls = vls.stream().filter(vl -> vl.getPriority() < flow.getPriority()).collect(Collectors.toList());
+        
+        double bandwidth = network.getBandwidth();
+        double portLatency = port.getPort().getDevice().getInternalLatency();
+        double vlLatency = 0.0;
+        
+        if (higherPrioVls == null || higherPrioVls.isEmpty()) {
+            //For high prio frame: latency is port latency + one lower prio frame (with max length)
+            int maxLength = 0;
+            final List<Flow> lowerPrioVls = vls.stream().filter(vl -> vl.getPriority() > flow.getPriority()).collect(Collectors.toList());
+            for (final var lowerPrioVl : lowerPrioVls) {
+                if (lowerPrioVl.getMaxLenght() > maxLength) {
+                    maxLength = lowerPrioVl.getMaxLenght();
+                }
+            }
+            vlLatency = maxLength/bandwidth;
+            
+        } else {
+            //service curve: R-rate
+            // latency: frameLength/(R-rate)
+            int sumLength = 0;
+            double sumRate = 0.0;
+            for (final var higherPrioVl : higherPrioVls) {
+                double c = higherPrioVl.getMaxLenght()/bandwidth;
+                double t = higherPrioVl.getMinRetransmissionInterval();
+                double rate = (c)/(t) * bandwidth;
+                sumLength += higherPrioVl.getMaxLenght();
+                sumRate += rate;
+            }
+            vlLatency = sumLength / (bandwidth - sumRate);
+        }
+        return portLatency + vlLatency;
+    }
+
+    private static Set<Flow> getVLsOnPort(final OutPort port, final Network network) {
+        final Set<Flow> result = new HashSet<>();
+        if (port.getPort() instanceof NetworkInterface) {
+            for (final VirtualLink vl : ((NetworkInterface)(port.getPort())).getVirtualLinks()) {
+                result.add(vl);
+            }
+        } else {
+            for (final var flow : network.getFlows()) {
+                for (final var path : flow.getPaths()) {
+                    for (final var link : path.getLinks()) {
+                        if (link.getSrcPort() == port) {
+                            result.add(flow);
+                        }
+                    }
+                }
+            }
+         
+        }
+        return result;
+    }
 }
